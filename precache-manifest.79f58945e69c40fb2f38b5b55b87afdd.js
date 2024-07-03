0bcc231cf649ab0cecc94873bcb9a9645bcbb926
Tomas van Rensburg <60756248+tomasvrensburg@users.noreply.github.com>
2024-07-03 09:16:58 +0200

    Add files via upload

---
+++ precache-manifest.79f58945e69c40fb2f38b5b55b87afdd.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ac7f418fda6faf1ea6088c891e3b2b",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/index.html"
+  },
+  {
+    "revision": "5f7a5019ebd9de1d511b",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/css/main.f3d573a6.chunk.css"
+  },
+  {
+    "revision": "7340f1b3e38499d06bf7",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/js/2.8856fc24.chunk.js"
+  },
+  {
+    "revision": "9c0c47d34de10fa07a7c",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/js/3.cf3acbb1.chunk.js"
+  },
+  {
+    "revision": "5f7a5019ebd9de1d511b",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/js/main.a8b84a37.chunk.js"
+  },
+  {
+    "revision": "e80eeaa2f054b2caabb7",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/js/runtime~main.f24ce0c9.js"
+  },
+  {
+    "revision": "9a5104c4aa9752952177f11f3aae2717",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/customiseIcon.9a5104c4.png"
+  },
+  {
+    "revision": "12984f69934f84d6b732b24a95eb1529",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/designImage.12984f69.jpeg"
+  },
+  {
+    "revision": "fdebdb99cec3f7d0df72857d0ee7b3d8",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/developmentImage.fdebdb99.jpeg"
+  },
+  {
+    "revision": "d7bf04007f4d7bcfd3900ca91ffc1c6a",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/domainIcon.d7bf0400.png"
+  },
+  {
+    "revision": "08cddbd966cd2c5f3f12857791aff4ce",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/heroSectionMockups.08cddbd9.jpeg"
+  },
+  {
+    "revision": "1fa2b0dded264e2c4e49e766c4d93488",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/heroSectionMockupsMedium.1fa2b0dd.jpeg"
+  },
+  {
+    "revision": "aa3dac1fc7fd1b40c63c46ced279ee14",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/heroSectionMockupsSmall.aa3dac1f.jpeg"
+  },
+  {
+    "revision": "dc3a8a9d986332f0b021e506fd8e3812",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/inviteImage.dc3a8a9d.jpeg"
+  },
+  {
+    "revision": "9539736da7cbf70c892b50255da4a812",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/mockupOne.9539736d.jpeg"
+  },
+  {
+    "revision": "37f113194f8a53121b6365a49540389b",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/mockupThree.37f11319.jpeg"
+  },
+  {
+    "revision": "cfaf08cd0618463a9c7d0eb3f4684693",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/mockupTwo.cfaf08cd.jpeg"
+  },
+  {
+    "revision": "7d155f978de8210474b7289ab7272271",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/payfastLogo.7d155f97.png"
+  },
+  {
+    "revision": "6af8f59feb4da9a9551c72aadea397d9",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/projectMockupOne.6af8f59f.jpeg"
+  },
+  {
+    "revision": "46e94bf0322ec61b2d65702dc525c55d",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/rsvpIcon.46e94bf0.png"
+  },
+  {
+    "revision": "126b940a3cca6d9da82948a685a93273",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/testimonialPhotoOne.126b940a.jpeg"
+  },
+  {
+    "revision": "8f0a4b55bd61d5f082ac819abf71540f",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/testimonialPhotoThree.8f0a4b55.jpeg"
+  },
+  {
+    "revision": "4d119aa0f58cf464345e4f2a549f147a",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/testimonialPhotoTwo.4d119aa0.jpeg"
+  },
+  {
+    "revision": "4ece46713af66375ad7d86146aeb17b5",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/updateIcon.4ece4671.png"
+  },
+  {
+    "revision": "43783a8e4de5b1840c314b9ae3cab7bf",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/wedifyLogo.43783a8e.jpeg"
+  },
+  {
+    "revision": "1756301380ad92d185dfb599acdf2538",
+    "url": "%3Cgithub-username%3E.github.io/%3Crepo-name%3E/static/media/wedifyLogoShort.17563013.jpeg"
+  }
+]);
